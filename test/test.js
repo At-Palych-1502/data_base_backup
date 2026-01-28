185cd52e66d9f72a0aa990f7a6ad72771744e61d
Максим <rmd09@yandex.ru>
2026-01-29 00:23:21 +0300

    .

--- test/test.js
+++
@@ -1,9 +0,0 @@
-const test = require("./instance/canteen.json");
-const readline = require('readline');
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-let flag = false;
-rl.question('Your Name: ', (answer) => {
-    console.log(test.q);
-    flag = true;
-    rl.close();
-});
